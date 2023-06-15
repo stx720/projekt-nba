6c9493670e5307720fe32ff3e7507f768e8537ed
stx720 <michal.obrebski2002@gmail.com>
2023-06-16 00:43:01 +0200

    Wyszukiwanie
    Dodanie możliwości wyszukiwania z API razem z kontrolą czy pole jest puste(alert toast). Do ostylowania są wyniki wyszukiwania jak i samo pole wyszukiwania z przyciskiem

--- pages/search.js
+++ pages/search.js
@@ -4,10 +4,31 @@ import Link from "next/link";
 import Image from "next/image";
 import NextNProgress from "nextjs-progressbar";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
-export default function Home({ search }) {
+export default function Home({ teams }) {
   const router = useRouter();
-  console.log(search);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    if (!searchTerm) {
+      toast.error('Wprowadź imię lub nazwisko zawodnika');
+      return;
+    }
+
+    const odpSearch = await fetch(
+      `https://www.balldontlie.io/api/v1/players?search=${searchTerm}`
+    );
+    const search = await odpSearch.json();
+
+    setSearchResults(search.data || []);
+  };
+  console.log(teams);
   return (
     <div className={styles.container}>
       <Head>
@@ -25,16 +46,36 @@ export default function Home({ search }) {
           </b>
         </p>
         <h1 className={styles.nagIndex}>Wyszukaj zawodnika:</h1>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Szukaj zawodnika..."
+            className={styles.inputSearch}
+          />
+          <button className={styles.buttonSearch} type="submit">Wyszukaj</button>
+        </form>
+        {searchResults.length > 0 && (
+          <ul className={styles.ulSearch}>
+            {searchResults.map((player) => (
+              <li key={player.id}>
+                {player.first_name} {player.last_name}
+              </li>
+            ))}
+          </ul>
+        )}
       </main>
+      <ToastContainer />
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const odpSearch = await fetch("https://www.balldontlie.io/api/v1/players");
-  const search = await odpSearch.json();
+  const odp = await fetch("https://www.balldontlie.io/api/v1/teams");
+  const teams = await odp.json();
 
   return {
-    props: { search },
+    props: { teams },
   };
 }
